0f6beb53cfdf52228d5da26a4c35d4b4a476e4fb
Speedy-Vongofast <sonic222333@gmail.com>
2019-01-08 10:35:18 +0100

    Code de base des bouttons suivant et précédent

--- js/calendrier.js
+++ js/calendrier.js
@@ -1,11 +1,24 @@
 let openedTab = ""
+let openedSeason = null
+const saisons = ["printemps", "ete", "automne", "hiver"]
+const styleSaisons = ["prin", "ete", "aut", "hiv"]
 
-function showSeason(event, tab) {
+function showSeason(event, tab, nbr) {
+    // Affichage des divs sur la page
     document.getElementById("pageSaisons").style.display = "none"
     document.getElementById("nav").style.display = "block"
     document.getElementById(tab).style.display = "grid"
     
     openedTab = tab
+
+    openedSeason = nbr
+
+
+    document.getElementById("suivant").setAttribute("src", "img/saisons/"+saisons[nbr+1]+".svg")
+    document.getElementById("suivant").classList.add(styleSaisons[nbr+1])
+
+    document.getElementById("precedent").setAttribute("src", "img/saisons/"+saisons[nbr-1]+".svg")
+    document.getElementById("precedent").classList.add(styleSaisons[nbr-1])
 }
 
 function closeSeason(event){
@@ -14,4 +27,12 @@ function closeSeason(event){
     document.getElementById(openedTab).style.display = "none"
 
     openedTab = ""
+}
+
+function nextSeason(event){
+
+}
+
+function previousSeason(event){
+
 }
