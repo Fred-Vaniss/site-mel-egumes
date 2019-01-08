f83a4dff768b2a1e6bda2ab1967b2fad068940c3
Speedy-Vongofast <sonic222333@gmail.com>
2019-01-08 11:24:39 +0100

    Calendrier: changement des styles des bouttons suivant et précédent

--- js/calendrier.js
+++ js/calendrier.js
@@ -1,7 +1,10 @@
-let openedTab = ""
-let openedSeason = null
-const saisons = ["printemps", "ete", "automne", "hiver"]
-const styleSaisons = ["prin", "ete", "aut", "hiv"]
+let openedTab
+let openedSeason
+let nextNbr
+let prevNbr
+const saisons =         ["printemps",   "ete",      "automne",  "hiver"]
+const styleSaisons =    ["prin",        "ete",      "aut",      "hiv"]
+const idSaisons =       ["saison1",     "saison2",  "saison3",  "saison4"]
 
 function showSeason(event, tab, nbr) {
     // Affichage des divs sur la page
@@ -13,12 +16,24 @@ function showSeason(event, tab, nbr) {
 
     openedSeason = nbr
 
+    if (nbr == 3) {
+        nextNbr = 0
+    } else {
+        nextNbr = nbr+1
+    }
 
-    document.getElementById("suivant").setAttribute("src", "img/saisons/"+saisons[nbr+1]+".svg")
-    document.getElementById("suivant").classList.add(styleSaisons[nbr+1])
+    if (nbr == 0) {
+        prevNbr = 3
+    } else {
+        prevNbr = nbr-1
+    }
 
-    document.getElementById("precedent").setAttribute("src", "img/saisons/"+saisons[nbr-1]+".svg")
-    document.getElementById("precedent").classList.add(styleSaisons[nbr-1])
+
+    document.getElementById("suivant").setAttribute("src", "img/saisons/"+saisons[nextNbr]+".svg")
+    document.getElementById("suivant").classList.add(styleSaisons[nextNbr])
+
+    document.getElementById("precedent").setAttribute("src", "img/saisons/"+saisons[prevNbr]+".svg")
+    document.getElementById("precedent").classList.add(styleSaisons[prevNbr])
 }
 
 function closeSeason(event){
@@ -27,6 +42,9 @@ function closeSeason(event){
     document.getElementById(openedTab).style.display = "none"
 
     openedTab = ""
+
+    document.getElementById("suivant").classList.remove(styleSaisons[nextNbr])
+    document.getElementById("precedent").classList.remove(styleSaisons[prevNbr])
 }
 
 function nextSeason(event){
