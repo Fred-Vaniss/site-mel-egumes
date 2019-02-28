a94712482cbcd68cc6c8e8e6f806ce56d0fb831e
Fred-Vaniss <46893914+Fred-Vaniss@users.noreply.github.com>
2019-02-28 16:37:05 +0100

    Menu: ajout d'une animation sur le menu du boutton burger

--- js/menu.js
+++ js/menu.js
@@ -1,15 +1,18 @@
 let menu = document.getElementById("menu")
 let menuButton = document.getElementsByClassName("menuButton")[0]
+let hamburger = document.getElementsByClassName("hamburger")[0]
 let menuDisp = false
 
 function showMenu(){
     console.log("click")
     if (menuDisp === false){
         menu.classList.add("showNav");
+        hamburger.classList.add("is-active")
         document.addEventListener("click", outsideClose);
         menuDisp = true;
     } else {
         menu.classList.remove("showNav")
+        hamburger.classList.remove("is-active")
         document.removeEventListener("click", outsideClose);
         menuDisp = false;
     }
@@ -19,6 +22,7 @@ function outsideClose(event){
     console.log("click")
     if (event.composedPath().includes(menu) === false && event.composedPath().includes(menuButton) === false){
         menu.classList.remove("showNav");
+        hamburger.classList.remove("is-active")
         document.removeEventListener("click", outsideClose);
         menuDisp = false;
     }
