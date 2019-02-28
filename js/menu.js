f575d809b6c1ee611a523b20e428c320fb7121bf
Fred-Vaniss <46893914+Fred-Vaniss@users.noreply.github.com>
2019-02-28 15:39:18 +0100

    Menu: ajout d'une fonction pour fermer le menu en appuyant à l'extérieur

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,25 @@
 let menu = document.getElementById("menu")
+let menuButton = document.getElementsByClassName("menuButton")[0]
+let menuDisp = false
 
 function showMenu(){
-    menu.classList.toggle("showNav")
+    console.log("click")
+    if (menuDisp === false){
+        menu.classList.add("showNav");
+        document.addEventListener("click", outsideClose);
+        menuDisp = true;
+    } else {
+        menu.classList.remove("showNav")
+        document.removeEventListener("click", outsideClose);
+        menuDisp = false;
+    }
+}
+
+function outsideClose(event){
+    console.log("click")
+    if (event.composedPath().includes(menu) === false && event.composedPath().includes(menuButton) === false){
+        menu.classList.remove("showNav");
+        document.removeEventListener("click", outsideClose);
+        menuDisp = false;
+    }
 }
